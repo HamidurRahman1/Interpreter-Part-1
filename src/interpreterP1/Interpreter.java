035f35b290600f675601b4e8119c99977cce4f37
Hamidur Rahman <Hamidur0405@gmail.com>
2020-04-19 22:57:55 -0400

    testing with professor interpreter

--- src/interpreterP1/Interpreter.java
+++ src/interpreterP1/Interpreter.java
@@ -15,21 +15,26 @@ public abstract class Interpreter extends Parser
 	*/
     public static void main(String argv[])
     {
-        setIO("/Users/hamidurrahman/Downloads/GitHub/Project--Interpreter-Part-1/src/inputs/in2.txt",
-                "/Users/hamidurrahman/Downloads/GitHub/Project--Interpreter-Part-1/src/outputs/out2.txt");
+//        setIO( argv[0], argv[1] );
+        setIO("/Users/hamidurrahman/Downloads/GitHub/Project--Interpreter-Part-1/src/inputs/in1.txt",
+                "/Users/hamidurrahman/Downloads/GitHub/Project--Interpreter-Part-1/src/outputs/out1.txt");
         setLex();
 
+        getToken();
+        FunDefList funDefList = funDefList();
+        if ( ! t.isEmpty() )
+            errorMsg(0);
+        closeIO();
+//            setIO( argv[2], argv[3] );
+        setIO("/Users/hamidurrahman/Downloads/GitHub/Project--Interpreter-Part-1/src/inputs/in2.txt",
+                "/Users/hamidurrahman/Downloads/GitHub/Project--Interpreter-Part-1/src/outputs/out2.txt");
         getToken();
         Exp exp = exp();
         if ( ! t.isEmpty() )
             displayln(t + "  -- unexpected symbol");
-        else if ( ! syntaxErrorFound )
-        {
-            Val v = exp.Eval(new HashMap<String, Val>());  // evaluate the given expression
-            if ( v != null )
-                System.out.println( v.toString() + " -> " + v.getClass() );   // display the value on the terminal
-        }
-
+        Val v = exp.Eval(new HashMap<String,Val>());  // evaluate the given expression
+        if ( v != null )
+            System.out.println( v.toString() );   // display the value on the terminal
         closeIO();
     }
 }
