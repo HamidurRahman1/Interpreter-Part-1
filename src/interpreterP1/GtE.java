1fc84765491a368ba9b0eea355e791fd71e059b1
Hamidur Rahman <Hamidur0405@gmail.com>
2020-04-23 01:16:37 -0400

    GtE complete

--- src/interpreterP1/GtE.java
+++ src/interpreterP1/GtE.java
@@ -1,77 +1,80 @@
 
 package interpreterP1;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
-class GtE extends FunExp
+public class GtE extends FunExp
 {
-    GtE(ExpList e)
+    public GtE(ExpList e)
     {
         expList = e;
     }
 
-    String getFunOp()
+    public String getFunOp()
     {
         return ">";
     }
 
     @Override
-    Val Eval(Map<String, Val> valMap)
+    public Val Eval(Map<String, Val> valMap)
     {
-        if(expList.getClass() == EmptyExpList.class) return new BoolVal(true);
+        valMap.put(getFunOp(), new BoolVal(true));
 
-        NonEmptyExpList ne = (NonEmptyExpList)expList;
-        ArrayList<Val> e = new ArrayList<>();
+        if(expList instanceof EmptyExpList) return valMap.get(getFunOp());
 
-        while (ne.expList != null)
+        NonEmptyExpList nonEmptyExpList = (NonEmptyExpList)expList;
+        List<Val> gtList = new LinkedList<>();
+
+        while (nonEmptyExpList.expList != null)
         {
-            Val v = ne.exp.Eval(valMap);
-            e.add(v);
-            if(ne.expList.getClass() == NonEmptyExpList.class) ne = (NonEmptyExpList)ne.expList;
+            gtList.add(nonEmptyExpList.exp.Eval(valMap));
+            if(nonEmptyExpList.expList instanceof NonEmptyExpList)
+                nonEmptyExpList = (NonEmptyExpList)nonEmptyExpList.expList;
             else break;
         }
 
-        if(e.size() <= 1)
-        {
-            return new BoolVal(true);
-        }
+        if(gtList.size() <= 1) return valMap.get(getFunOp());
         else
         {
-            FloatVal v;
-            if(e.get(0).getClass() == IntVal.class)
+            FloatVal fG1;
+            if(gtList.get(0) instanceof IntVal) fG1 = new FloatVal(((IntVal)gtList.get(0)).val);
+            else if(gtList.get(0) instanceof FloatVal) fG1 = (FloatVal) gtList.get(0);
+            else
             {
-                v = new FloatVal(((IntVal)e.get(0)).val);
+                System.out.println("Error: "+getFunOp()+" operator cannot be applied to " + gtList.get(0));
+                return null;
             }
-            else v = (FloatVal) e.get(0);
-            boolean q = true;
-            for(int i = 1; i < e.size(); i++)
+
+            boolean gFlag = true;
+
+            for(int i = 1; i < gtList.size(); i++)
             {
-                Class c = e.get(i).getClass();
-                if(c == IntVal.class || c == FloatVal.class)
+                if(gtList.get(i) instanceof IntVal || gtList.get(i) instanceof FloatVal)
                 {
-                    FloatVal o;
-                    if(e.get(i).getClass() ==  FloatVal.class)
-                        o = new FloatVal(((FloatVal)e.get(i)).val);
-                    else o = new FloatVal(((IntVal)e.get(i)).val);
-                    if(v.val > o.val)
+                    FloatVal next;
+                    if(gtList.get(i) instanceof FloatVal) next = new FloatVal(((FloatVal)gtList.get(i)).val);
+                    else if(gtList.get(i) instanceof IntVal) next = new FloatVal(((IntVal)gtList.get(i)).val);
+                    else
                     {
-                        v = o;
-                        q = true;
-                        continue;
+                        System.out.println("Error: "+getFunOp()+" operator cannot be applied to " + gtList.get(0));
+                        return null;
+                    }
+                    if(fG1.val > next.val)
+                    {
+                        fG1 = next;
+                        gFlag = true;
                     }
                     else
                     {
-                        v = o;
-                        q = false;
+                        fG1 = next;
+                        gFlag = false;
                     }
                 }
             }
-            if(q)
-            {
-                return new BoolVal(true);
-            }
-            return new BoolVal(false);
+            if(gFlag) return valMap.replace(getFunOp(), new BoolVal(true));
+            return valMap.replace(getFunOp(), new BoolVal(false));
         }
     }
 }
