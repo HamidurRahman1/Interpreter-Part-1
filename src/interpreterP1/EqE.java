cb636b580cfacf8123fe576f03c6a541b51c13f7
Hamidur Rahman <Hamidur0405@gmail.com>
2020-04-23 01:01:50 -0400

    EqE complete

--- src/interpreterP1/EqE.java
+++ src/interpreterP1/EqE.java
@@ -1,79 +1,96 @@
 
 package interpreterP1;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
-class EqE extends FunExp
+public class EqE extends FunExp
 {
-    EqE(ExpList e)
+    public EqE(ExpList e)
     {
         expList = e;
     }
 
-    String getFunOp()
+    public String getFunOp()
     {
         return "=";
     }
 
     @Override
-    Val Eval(Map<String, Val> valMap)
+    public Val Eval(Map<String, Val> valMap)
     {
-        if(expList.getClass() == EmptyExpList.class) return new BoolVal(true);
+        valMap.put(getFunOp(), new BoolVal(true));
 
-        NonEmptyExpList ne = (NonEmptyExpList)expList;
-        ArrayList<Val> e = new ArrayList<>();
-
-        while (ne.expList != null)
-        {
-            Val v = ne.exp.Eval(valMap);
-            e.add(v);
-            if(ne.expList.getClass() == NonEmptyExpList.class) ne = (NonEmptyExpList)ne.expList;
-            else break;
-        }
-
-        if(e.size() <= 1) return new BoolVal(true);
+        if(expList instanceof EmptyExpList) return valMap.get(getFunOp());
         else
         {
-            FloatVal v = null;
-            if(e.get(0).getClass() == IntVal.class) v = new FloatVal(((IntVal)e.get(0)).val);
-            else if(e.get(0).getClass() == FloatVal.class) v = (FloatVal) e.get(0);
-            else if(e.get(0).getClass() == PairVal.class)
+            NonEmptyExpList nonEmptyExpList = (NonEmptyExpList)expList;
+            List<Val> eVal = new LinkedList<>();
+
+            while (nonEmptyExpList.expList != null)
             {
-                Val f = ((PairVal)e.get(0)).first;
-                Val s = ((PairVal)e.get(0)).second;
-                for(int i = 1; i < e.size(); i++)
-                {
-                    PairVal p = (PairVal) e.get(i);
-                    if(!(f.toString().equals(p.first.toString()) && s.toString().equals(p.second.toString())))
-                        return new BoolVal(false);
-                }
-                return new BoolVal(true);
+                eVal.add(nonEmptyExpList.exp.Eval(valMap));
+                if(nonEmptyExpList.expList instanceof NonEmptyExpList)
+                    nonEmptyExpList = (NonEmptyExpList)nonEmptyExpList.expList;
+                else break;
             }
-            boolean q = true;
-            for(int i = 1; i < e.size(); i++)
+
+            if(eVal.size() <= 1) return valMap.get(getFunOp());
+            else
             {
-                Class c = e.get(i).getClass();
-                if(c == IntVal.class || c == FloatVal.class)
+                FloatVal eq1 = null;
+                if(eVal.get(0) instanceof IntVal) eq1 = new FloatVal(((IntVal)eVal.get(0)).val);
+                else if(eVal.get(0) instanceof FloatVal) eq1 = (FloatVal) eVal.get(0);
+                else if(eVal.get(0) instanceof PairVal)
                 {
-                    FloatVal o = null;
-                    if(e.get(i).getClass() ==  FloatVal.class) o = new FloatVal(((FloatVal)e.get(i)).val);
-                    else if(e.get(i).getClass() ==  IntVal.class) o = new FloatVal(((IntVal)e.get(i)).val);
-                    if(v.val == o.val)
+                    Val first = ((PairVal)eVal.get(0)).first;
+                    Val second = ((PairVal)eVal.get(0)).second;
+                    for(int i = 1; i < eVal.size(); i++)
                     {
-                        v = o;
-                        q = true;
-                        continue;
+                        PairVal pairVal = (PairVal) eVal.get(i);
+                        if(!(first.toString().equals(pairVal.first.toString()) && second.toString().equals(pairVal.second.toString())))
+                        {
+                            valMap.replace(getFunOp(), new BoolVal(false));
+                            return valMap.get(getFunOp());
+                        }
                     }
-                    else
+                    valMap.replace(getFunOp(), new BoolVal(true));
+                    return valMap.get(getFunOp());
+                }
+                else
+                {
+                    System.out.println("Error: "+getFunOp()+" operator cannot be applied to " + eVal.get(0));
+                    return null;
+                }
+                boolean eqFlag = true;
+                for(int i = 1; i < eVal.size(); i++)
+                {
+                    if(eVal.get(i) instanceof IntVal || eVal.get(i) instanceof FloatVal)
                     {
-                        v = o;
-                        q = false;
+                        FloatVal next = null;
+                        if(eVal.get(i) instanceof FloatVal) next = new FloatVal(((FloatVal)eVal.get(i)).val);
+                        else if(eVal.get(i) instanceof IntVal) next = new FloatVal(((IntVal)eVal.get(i)).val);
+                        else
+                        {
+                            System.out.println("Error: "+getFunOp()+" operator cannot be applied to " + eVal.get(i));
+                            return null;
+                        }
+                        if(eq1.val == next.val)
+                        {
+                            eq1 = next;
+                            eqFlag = true;
+                        }
+                        else
+                        {
+                            eq1 = next;
+                            eqFlag = false;
+                        }
                     }
                 }
+                if(eqFlag) return valMap.replace(getFunOp(), new BoolVal(true));
+                return valMap.replace(getFunOp(), new BoolVal(false));
             }
-            if(q) return new BoolVal(true);
-            return new BoolVal(false);
         }
     }
 }
